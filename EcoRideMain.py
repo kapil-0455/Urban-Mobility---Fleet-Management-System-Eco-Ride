aa62dfcfc6b25d45b1029212b4d7b82813cf31c5
kapil-0455 <kapil0455cu@gmail.com>
2026-08-04 11:57:40 +0530

    update[kapil]: done uc2 task

--- EcoRideMain.py
+++ EcoRideMain.py
@@ -1,8 +1,20 @@
+from vehicle import Vehicle
+
+
 class EcoRideMain:
     def __init__(self) -> None:
         pass
     def run(self) -> None:
         print("Welcome to Eco-Ride Urban Mobility System")
+
+        car = Vehicle("001", "Scorpio S11", 30, "Good", 2500)
+        print("Testing Vehicle details:")
+        print(car)
+
+        car.set_battery_percentage(80)
+        car.set_rental_price(3000)
+        print("\nAfter Update:")
+        print(car)
 
 
 if __name__ == "__main__":
--- vehicle.py
+++ vehicle.py
@@ -1,12 +1,42 @@
 class Vehicle:
-    def __init__(self, vehicle_id: str, model: str, battery_percentage: float):
-        self.vehicle_id = vehicle_id
-        self.model = model
-        self.battery_percentage = battery_percentage
+    def __init__(self, vehicle_id, model, battery_percentage, maintenance_status, rental_price):
+        self.__vehicle_id = vehicle_id
+        self.__model = model
+        self.set_battery_percentage(battery_percentage)
+        self.__maintenance_status = maintenance_status
+        self.__rental_price = rental_price
+
+    # Getters
+    def get_vehicle_id(self):
+        return self.__vehicle_id
+
+    def get_model(self):
+        return self.__model
+
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+
+    def get_rental_price(self):
+        return self.__rental_price
+
+    # Setters
+    def set_battery_percentage(self, percentage):
+        if 0 <= percentage <= 100:
+            self.__battery_percentage = percentage
+        else:
+            raise ValueError("Battery percentage must be between 0 and 100.")
+
+    def set_maintenance_status(self, status):
+        self.__maintenance_status = status
+
+    def set_rental_price(self, price):
+        if price >= 0:
+            self.__rental_price = price
+        else:
+            raise ValueError("Rental price cannot be negative.")
 
     def __str__(self):
-        return f"Vehicle [ID: {self.vehicle_id}, Model: {self.model}, Battery: {self.battery_percentage}%]"
-
-if __name__ == '__main__':
-    car = Vehicle('Scorpio' , 'S11' , '100')
-    print(car)
+        return f"Vehicle [ID: {self.__vehicle_id}, Model: {self.__model}, Battery: {self.__battery_percentage}%, Maintenance: {self.__maintenance_status}, Rental Price: {self.__rental_price}]"
